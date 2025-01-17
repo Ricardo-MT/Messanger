b01848e4bd7bb21ec94dfa4e0b5ff8b7db698e95
Ricardo Mejias Travieso <rikr2.mt@gmail.com>
2025-01-17 16:23:12 +0100

    feat: profile image on group chats

--- src/modules/universe/chat/chatComponent/ChatComponent.tsx
+++ src/modules/universe/chat/chatComponent/ChatComponent.tsx
@@ -6,7 +6,8 @@ import { universeState } from "../../universeSlice";
 import { useChatCompose } from "./useChatCompose";
 import { ChatPicture } from "../components/ChatPicture";
 import { BiChevronLeft, BiSend } from "react-icons/bi";
-import { useRef } from "react";
+import { useMemo, useRef } from "react";
+import { Profile } from "../../../../interfaces/profile";
 
 type Props = {
   chat?: Chat | null;
@@ -18,6 +19,20 @@ export const ChatComponent = ({ chat, messages, onGoBack }: Props) => {
   const { profile } = useAppSelector(universeState);
   const { text, setText, submit } = useChatCompose();
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const listItems: (Message | string)[] = useMemo(() => {
+    const items: (Message | string)[] = [];
+    let lastDate = messages[0]?.timestamp.toDateString() ?? "";
+    messages.forEach((message) => {
+      const date = message.timestamp.toDateString();
+      if (date !== lastDate) {
+        items.push(lastDate);
+        lastDate = date;
+      }
+      items.push(message);
+    });
+    items.push(lastDate);
+    return items;
+  }, [messages]);
 
   if (!chat) {
     return <div>Select a chat</div>;
@@ -43,14 +58,36 @@ export const ChatComponent = ({ chat, messages, onGoBack }: Props) => {
         <ChatPicture chat={chat} currentUserId={profile!.id} />
         {chat.members.find((member) => member.id !== profile?.id)?.name}
       </div>
-      <div className={css.chatBody}>
-        {messages.map((message) => (
-          <OneMessage
-            key={message.id}
-            message={message}
-            mine={message.senderId === profile?.id}
-          />
-        ))}
+      <div className={`${css.chatBody} ${chat.isGroup ? css.chatBody : ""}`}>
+        {listItems.map((message, i) => {
+          if (typeof message === "string") {
+            return (
+              <div
+                key={message}
+                className={`dateSeparator ${css.dateSeparator}`}
+              >
+                {message}
+              </div>
+            );
+          }
+          const thisProfile =
+            message.senderId === profile?.id
+              ? profile!
+              : chat.members.find((member) => member.id === message.senderId)!;
+          const shouldShowImage =
+            chat.isGroup &&
+            message.senderId !== profile?.id &&
+            !(i > 0 && listItems[i - 1].senderId === message.senderId);
+          return (
+            <OneMessage
+              key={message.id}
+              message={message}
+              mine={message.senderId === profile?.id}
+              profile={thisProfile}
+              shouldShowImage={shouldShowImage}
+            />
+          );
+        })}
       </div>
       <form
         className={`${css.chatInputContainer} chatInputContainer`}
@@ -84,20 +121,33 @@ export const ChatComponent = ({ chat, messages, onGoBack }: Props) => {
 export const OneMessage = ({
   message,
   mine,
+  profile,
+  shouldShowImage,
 }: {
   message: Message;
   mine: boolean;
+  profile: Profile;
+  shouldShowImage?: boolean;
 }) => {
   return (
     <div
-      className={`message ${css.message} ${
+      className={`messageContainer ${css.messageContainer} ${
         mine ? `myMessage ` + css.myMessage : `otherMessage ` + css.otherMessage
       }`}
     >
-      <span>{message.text}</span>
-      <span className="timestamp">
-        {message.timestamp.toLocaleTimeString().substring(0, 5)}
-      </span>
+      <span
+        className={`messageProfileImage ${css.messageProfileImage}`}
+        data-profile-image={(shouldShowImage && profile.avatar) || ""}
+        style={{
+          "--profile-image": `url(${profile.avatar})`,
+        }}
+      ></span>
+      <div className={`messageContent ${css.messageContent}`}>
+        <span>{message.text}</span>
+        <span className="timestamp">
+          {message.timestamp.toLocaleTimeString().substring(0, 5)}
+        </span>
+      </div>
     </div>
   );
 };
