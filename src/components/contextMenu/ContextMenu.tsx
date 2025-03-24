14201114ee830f992b3c086cfe0bc76a0eb4e942
Ricardo Mejias Travieso <rikr2.mt@gmail.com>
2025-03-24 12:55:29 +0100

    fix: context menu

--- src/components/contextMenu/ContextMenu.tsx
+++ src/components/contextMenu/ContextMenu.tsx
@@ -1,11 +1,9 @@
-import React, { CSSProperties, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { IconType } from "react-icons";
 import { LongPressReactEvents, useLongPress } from "use-long-press";
 import css from "./ContextMenu.module.css";
 import ReactDOM from "react-dom";
 import { AnimatePresence, motion } from "motion/react";
-
-const extraSpace = 4;
 
 type Props = {
   options: ContextMenuOption[];
@@ -17,25 +15,13 @@ type Props = {
 export const ContextMenu = ({ children, options }: Props) => {
   const [state, setState] = useState<{
     element: HTMLElement;
-    style: CSSProperties;
   } | null>(null);
   const onLongPressBinder = useLongPress((e) => {
     const { target } = e;
     const element = getParentMessageElement(target as HTMLElement);
 
-    const { top, left, height } = element.getBoundingClientRect();
     const cloned = element.cloneNode(true) as HTMLElement;
-    cloned.style.position = "absolute";
-    cloned.style.top = `${top}px`;
-    cloned.style.left = `${left}px`;
-
-    const newStyles: CSSProperties = {};
-    if (top < window.innerHeight / 2) {
-      newStyles.top = `${top + height / 2 + extraSpace}px`;
-    } else {
-      newStyles.bottom = `${window.innerHeight - top + extraSpace}px`;
-    }
-    setState({ element: cloned, style: newStyles });
+    setState({ element: cloned });
   });
 
   return (
@@ -47,7 +33,6 @@ export const ContextMenu = ({ children, options }: Props) => {
             <BackgroundPanel
               clonedElement={state.element}
               options={options}
-              optionsContainerStyles={state.style}
               onClose={() => setState(null)}
             />
           )}
@@ -61,37 +46,44 @@ export const ContextMenu = ({ children, options }: Props) => {
 type BackgroundPanelProps = {
   clonedElement: HTMLElement;
   options: ContextMenuOption[];
-  optionsContainerStyles: React.CSSProperties;
   onClose: () => void;
 };
 
 export const BackgroundPanel = ({
   clonedElement,
   options,
-  optionsContainerStyles,
   onClose,
 }: BackgroundPanelProps) => {
   const backgroundRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    backgroundRef.current?.appendChild(clonedElement);
+    backgroundRef.current?.insertBefore(
+      clonedElement,
+      backgroundRef.current.firstChild
+    );
   }, []);
 
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+      animate={{ backgroundColor: "var(--background-traslucent)" }}
       exit={{ opacity: 0 }}
       ref={backgroundRef}
       className={`${css.backgroundPanel} backgroundPanel`}
-      onClick={onClose}
+      onClick={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        const target = e.target;
+        if (target === backgroundRef.current) {
+          onClose();
+        }
+      }}
     >
       <motion.div
-        initial={{ scale: 0 }}
+        initial={{ scale: 0, transformOrigin: "top" }}
         animate={{ scale: 1 }}
         exit={{ scale: 0 }}
         className={css.contextMenuOptionsContainer}
-        style={optionsContainerStyles}
       >
         {options.map((option, index) => (
           <motion.div
@@ -100,8 +92,9 @@ export const BackgroundPanel = ({
             }}
             key={index}
             className={css.contextMenuOption}
-            onClick={(e) => {
+            onTap={(e) => {
               e.stopPropagation();
+              e.preventDefault();
               onClose();
               option.onClick();
             }}
